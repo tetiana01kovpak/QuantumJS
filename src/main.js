261b1d6b0013687579cf6cdc7ba6447d089ca4dd
Alina <a.slipenka@gmail.com>
2025-11-08 11:07:46 +0200

    Revert "furnitures_js"

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { loadFeedbacks, renderFeedbacks, initReviews } from './js/feedback';
 
 initReviews();
 import { toggleNavMenu } from './js/header';
-import './js/furniture-list.js';
 import './js/faq.js';
 import './js/order-modal.js';
